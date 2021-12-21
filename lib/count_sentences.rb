4db7e5333fb564103bcd44b02b27ade6706ae2ee
Roger Mendez <roger61087@gmail.com>
2021-12-21 18:30:57 -0500

    commit tree a3f320cba3371c3ebe7b3757653647bb6533e986

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,37 @@ require 'pry'
 
 class String
 
+  def report_on_self
+    "Self is: #{self}"
+  end
+
   def sentence?
+    self.end_with?(".")
+    
+    
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    split = self.split(/[.?!]\D\D/)
+    split.count
+
+
+
+    # binding.pry
 
   end
 end
+
+
+binding.pry
